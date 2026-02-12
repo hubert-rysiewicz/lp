1de59579b0bfcc0777ea358ed66b269e767516f0
Hubert <hubert@rysiewicz.com>
2026-02-12 23:27:46 +0000

    Changes

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,8 @@
 export default function DashboardPage() {
   return (
-    <div className="bg-red-500">
+    <div>
       <h1 className="text-2xl font-bold">Dashboard Home</h1>
       <p>If you can see this inside the drawer, it works.</p>
-      <button className="btn btn-primary">TEST BUTTON</button>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="bg-white min-h-screen flex items-center justify-center">
-      <h1 className="text-black text-4xl">Hello</h1>
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <Link href="dashboard">
+        <button className="btn btn-primary">Dashboard</button>
+      </Link>
     </div>
   );
 }
