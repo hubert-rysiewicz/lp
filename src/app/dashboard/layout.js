e394a9abd42f1f24ea66eb2970919eda6ad4f1d3
hubert-rysiewicz <hubert@rysiewicz.com>
2026-02-13 18:21:15 +0000

    Formatting & design

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -39,7 +39,7 @@ export default function DashboardLayout({ children }) {
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
-          <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64">
+          <div className="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-72">
             {/* Sidebar content here */}
             <ul className="menu w-full grow">
               {/* List item */}
@@ -57,7 +57,7 @@ export default function DashboardLayout({ children }) {
                     strokeWidth="2"
                     fill="none"
                     stroke="currentColor"
-                    className="my-1.5 inline-block size-4"
+                    className="my-1.5 inline-block size-8"
                   >
                     <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"></path>
                     <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
@@ -81,7 +81,7 @@ export default function DashboardLayout({ children }) {
                     strokeWidth="2"
                     fill="none"
                     stroke="currentColor"
-                    className="my-1.5 inline-block size-4"
+                    className="my-1.5 inline-block size-8"
                   >
                     <path d="M20 7h-9"></path>
                     <path d="M14 17H5"></path>
